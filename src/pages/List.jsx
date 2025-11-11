a545e1fb81cd946724d9d9390b05f672e6c18021
kostabkpark2 <kostabkpark2@gmail.com>
2025-11-11 10:05:02 +0900

    board list.jsx

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function List() {
@@ -7,14 +8,44 @@ function List() {
         function getData() {
             axios.get("http://localhost:8000/boards")
             .then((res)=>{
-                console.log(res.data);
                 setList(res.data);
             })
         }
         getData();
     }, []);
+    console.log(list);
     return (
-        <h2>List.jsx</h2>
+        <> 
+            <h2>List.jsx</h2>
+            <hr />
+            <table>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일자</th>
+                        <th>조회수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.map((board)=>(
+                        <tr key={board.id}>
+                            <td>{board.id}</td>
+                            <td>
+                                <Link to={{
+                                    pathname : `/boards/${board.id}`,
+                                }} >{board.title}
+                                </Link>
+                            </td>
+                            <td>{board.writer}</td>
+                            <td>{board.createdAt}</td>
+                            <td>{board.hits}</td>                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
 
